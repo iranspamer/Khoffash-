8b58a7b16a4ddb04eb3bd7a70764e3557db6f8a2
iranspamer <mahanabedini76@gmail.com>
2015-12-28 20:44:32 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
+   sudo_users = { 110626080 , 174784541 ,,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
@@ -228,20 +228,15 @@ function create_config( )
 An advance Administration bot based on yagop/telegram-bot 
 
 https://github.com/SEEDTEAM/TeleSeed
-
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@seyedan25 [Manager]
-
+@khoffash_Admin [Founder]
+@khoffash1234[Developer]
+@ADRIN437 [Manager]
+ 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+ADRIN
 
-Our channels
-@teleseedch [English]
+(Khoffashbot)
 ]],
     help_text = [[
 Commands list :
